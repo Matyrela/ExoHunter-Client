9296be09b8469c3400256ffc8fa8d177ced21185
floroldos <florencia.roldos@correo.ucu.edu.uy>
2025-10-04 17:17:24 -0300

    full

--- ExoHunter/src/app/app.routes.ts
+++ ExoHunter/src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
     {
-        path: '',
-        component: HomeComponent,
-        children: [
-            { path: '', pathMatch: 'full', redirectTo: 'home' },
-            {
-                path: '',
-                loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-                title: 'ExoHunter | Home'
-            },
-            { path: 'about', loadComponent: () => import('./pages/about-us/about-us.component').then(m => m.AboutComponent), title: 'About' }
-        ]
+        path: 'home',
+        loadComponent: () =>
+            import('./pages/home/home.component').then((m) => m.HomeComponent),
     },
-    { path: '**', redirectTo: '' }
+    {
+        path: 'about',
+        loadComponent: () =>
+            import('./pages/about-us/about-us.component').then((m) => m.AboutComponent),
+    },
+    {
+        path: 'explorer',
+        loadComponent: () =>
+            import('./pages/explorer/explorer.component').then((m) => m.ExplorerComponent),
+    }
 ];
