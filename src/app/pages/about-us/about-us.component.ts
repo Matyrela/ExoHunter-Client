cc0d3c3c64b84e7a7a843268f7398bc9a865f668
floroldos <florencia.roldos@correo.ucu.edu.uy>
2025-10-05 13:01:29 -0300

    cambios de about

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatIconModule, MatDividerModule, MatButtonModule, RouterModule],
+  imports: [MatIconModule, MatDividerModule, MatButtonModule, MatCardModule, CommonModule, RouterModule],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
